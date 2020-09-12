9d94bc114e11bcab48044dee5adac4908e6aa89d
Marilia Nunes Alves <nunes.marilia@gmail.com>
2020-09-12 15:54:34 -0300

    Initial commit

---
+++ src/main/java/com/mn/TestNumberMagic.java
@@ -0,0 +1,101 @@
+package com.mn;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.IntStream;
+
+public class TestNumberMagic {
+
+    static boolean testCousin   = true;
+    static int countCousin      = 0;
+    static int countNumberMagic = 0;
+
+    public static void main(String[] args){
+
+        System.out.println("*******************************************");
+        System.out.println("Resultado: "+validNumberMagic(args));
+        System.out.println("*******************************************");
+    }
+
+    public static int validNumberMagic(String[] param){
+        /*
+         * Validações de entrada
+         * - Números positivos
+         * - Números em conjuntos (2 elementos)
+         * - Números separados por ','
+         * - Conjuntos separados por ','
+         * - Número não inteiro
+         */
+        String parseParam       = param[0].toString();
+        Pattern filerRegx       = Pattern.compile("^\\[[ ]*(\\[(([ ]*[0-9]+[ ]*,[ ]*[0-9]+)[ ]*\\][ ]*[,]?[ ]*)+|[ ]*[0-9]+[ ]*,[ ]*[0-9]+[ ]*)+[ ]*\\]$");
+        Matcher regexMatcher    = filerRegx.matcher(parseParam);
+
+
+        if( !regexMatcher.matches() ){
+            System.out.println("Parêmetro de entrada inválido");
+            throw new IllegalArgumentException("Parêmetro de entrada inválido");
+        }
+
+        /* Formatação dos dados de entrada em lista */
+        parseParam  = parseParam.substring (0, parseParam.length() - 1);
+        parseParam  = parseParam.replace("[","");
+        parseParam  = parseParam.replace("]","\'");
+        parseParam  = parseParam+",";
+
+        String[] arrayParse  = parseParam.split("\',");
+
+        List<String> listPeriods = Arrays.asList(arrayParse);
+
+        countNumberMagic    = 0;
+
+        listPeriods.forEach(period->{
+            String[] arrayPeriod    = period.split(",");
+            int periodInitial       = Integer.parseInt(arrayPeriod[0]);
+            int periodFinal         = Integer.parseInt(arrayPeriod[1])+1;
+
+            if( periodFinal < periodInitial ){
+                // Lançar excpetion de não pode ser o fim menor que o inicio
+                System.out.println("Período: ["+periodInitial+','+periodFinal+"] encontra-se com inconformidade com a regra B < A ");
+                throw new IllegalArgumentException("Período: ["+periodInitial+','+periodFinal+"] encontra-se com inconformidade com a regra B < A ");
+            }
+
+            IntStream.range(periodInitial, periodFinal).forEach(numberInterval-> {
+                // Testar raiz
+                double resultSqrt    = Math.sqrt(numberInterval);
+
+                if((Math.pow(resultSqrt,2) == new Integer(numberInterval).doubleValue()) && isCousin(numberInterval) ){
+                    countNumberMagic++;
+                    System.out.println("Número: "+numberInterval+" - Raiz:"+resultSqrt);
+                }
+            });
+
+            System.out.println("*******************************************");
+        });
+
+        return countNumberMagic;
+    }
+
+    private static boolean isCousin(int numberTest) {
+        testCousin  = false;
+        countCousin = 0;
+
+        if( numberTest == 1 )
+            return testCousin;
+
+        IntStream.range(1, numberTest).forEach(numberCalc ->{
+
+            if (numberTest % numberCalc == 0)
+                countCousin++;
+        });
+
+        if( countCousin == 2 ) {
+            System.out.println("Primo:"+numberTest);
+            System.out.println("Contador:"+countCousin);
+            testCousin = true;
+        }
+        return testCousin;
+    }
+}
+
---
+++ src/test/java/com/mn/TestNumberMagicTest.java
@@ -0,0 +1,44 @@
+package com.mn;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class TestNumberMagicTest {
+    @Test
+    @DisplayName("Teste sucesso do número mágico utilizando o conjunto [[8,27],[49,49]]")
+    void testNumberMagicExampleSuccess() {
+        String[] param  = new String[1];
+        param[0]        = "[[8,27],[49,49]]";
+
+        int resultNumberMagic   = TestNumberMagic.validNumberMagic(param);
+        Assertions.assertEquals(3, resultNumberMagic);
+    }
+
+    @Test
+    @DisplayName("Teste sucesso parêmetro inválido: negativo - Entrada [[-8,27],[49,49]]")
+    void testParamInvalidNegative() {
+        String[] param  = new String[1];
+        param[0]        = "[[-8,27],[49,49]]";
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> TestNumberMagic.validNumberMagic(param));
+    }
+
+    @Test
+    @DisplayName("Teste sucesso parêmetro inválido: B < A - Entrada [[30,27]]")
+    void testParamInvalidBBeforeA() {
+        String[] param  = new String[1];
+        param[0]        = "[[30,27]]";
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> TestNumberMagic.validNumberMagic(param));
+    }
+
+    @Test
+    @DisplayName("Teste sucesso parêmetro inválido: Número não inteiro - Entrada [[30.5,27]]")
+    void testParamInvalidNumberNotInteger() {
+        String[] param  = new String[1];
+        param[0]        = "[[30.5,27]]";
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> TestNumberMagic.validNumberMagic(param));
+    }
+}
